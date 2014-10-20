89e8756f55fda83059486cdc56dedbc7f96212fc
Garret Cole <gcole@blackpixel.com>
2014-10-20 06:29:52 -0700

    updated logic for GUIComponent

--- Assets/Scripts/BattleSystem/GUI/CharacterGUIComponent.cs
+++ Assets/Scripts/BattleSystem/GUI/CharacterGUIComponent.cs
@@ -22,6 +22,7 @@ public class CharacterGUIComponent : MonoBehaviour {
 			characterTitle.text = mBattleEntity.character.displayName;
             hpSlider.maxValue = mBattleEntity.maxHP;
             hpSlider.minValue = 0;
+			hpSlider.value = mBattleEntity.currentHP;
         }
     }            
     
@@ -31,8 +32,47 @@ public class CharacterGUIComponent : MonoBehaviour {
             return;
         }
 
-        actionSlider.value = mBattleEntity.turnState.turnPercent;
+		UpdateActionSlider ();
+		UpdateHPSlider ();
+		UpdateResourceSlider ();
     }
 
+	private void UpdateResourceSlider() {
+		// TODO implement
+	}
+
+	/// <summary>
+	/// Updates the HP slider.
+	/// </summary>
+	private void UpdateHPSlider() {
+		// TODO provide yeilding animation, decelerating interporlators 
+		hpSlider.value = mBattleEntity.currentHP;
+	}
+
+	/// <summary>
+	/// Updates the action slider. to meet the four TurnState.Phase states
+	/// </summary>
+	private void UpdateActionSlider() {
+		switch (mBattleEntity.turnState.phase) {
+			// PREPARE -> animate 0 to 1 as completes
+		case TurnState.Phase.PREPARE:
+			actionSlider.value = mBattleEntity.turnState.turnPercent;
+			break;
+			// EXECUTING -> stays at 1
+		case TurnState.Phase.EXECUTE:
+			actionSlider.value = 1f;
+			break;
+			// RECOVER -> animate 1 to 0 as completes
+		case TurnState.Phase.RECOVER:
+			actionSlider.value = 1f - mBattleEntity.turnState.turnPercent;
+			break;
+			// awaiting input, at 0 
+		case TurnState.Phase.REQUIRES_INPUT:
+			actionSlider.value = 0f;
+			break;
+		default:
+			break;
+		}
+	}
 }
 
